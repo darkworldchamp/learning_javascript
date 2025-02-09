f1629df9ae0db69ab84692d4d494240cec2f74eb
Nikhil sharma <140893545+darkworldchamp@users.noreply.github.com>
2025-02-09 20:47:41 +0000

    basic

---
+++ basics/datatype_conversion.js
@@ -0,0 +1,30 @@
+let score = "22fsd";
+console.log(typeof score);
+console.log(score);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"22" is =>string
+//22 is =>number
+//"22fsd" is =>NAN
+
+let isloggedIn = 1;
+
+let booleanisloggedin = Boolean(isloggedIn);
+console.log(isloggedIn )
+console.log(typeof booleanisloggedin )
+console.log(booleanisloggedin )
+
+//boolean 1=> true , 0=> false
+
+let num = 11
+let string = String(num)
+console.log(typeof string)
+console.log(string)
+
+
+// whenever we are type converting a variable 
+// type in which it will be converted is written 
+// started by capital letter 
+// like => Number, string , Boolean
