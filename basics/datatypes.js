a8762366eacb3fa92b1746868b5fbde8312bf235
Nikhil sharma <140893545+darkworldchamp@users.noreply.github.com>
2025-02-09 20:28:41 +0000

    basic commit

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as newer version
+// alert(3+3) // we are using node js not browser
+
+console.log(3
+    +
+    3
+)// code readibility shoud be high
+
+let name ="nikhil"
+let age=21
+let isloggdIn=false
+let state ;
+//number => 2  to power 53
+//bigint
+//string => ""
+//boolean => true/ flase
+//null => standalone value
+// undefined =>
+// symbol =>unique
+
+//object
+
+console.log(typeof age) // to detect the type of variable
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const ID = 15209 // constant canot be altered
+let accountEmail="22bcs15209@cuchd.in" //mostly used 
+var accountpass="12345" // not used because of scope of this type can be altered anywhere declared so it is not preffered
+accountcity="kota" //can also be created like this
+
+let state
+
+// accountId= 2 //not allowed
+accountEmail="abc@gmial.com"
+accountpass="121212"
+
+console.log(ID)
+console.table([ID,accountEmail,accountpass,accountcity,state])
