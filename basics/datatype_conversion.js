85a3dde1d9f6162b5b9aaa629e7de14d9ce6d446
Nikhil sharma <140893545+darkworldchamp@users.noreply.github.com>
2025-02-10 10:26:31 +0000

    a

--- basics/datatype_conversion.js
+++ basics/datatype_conversion.js
@@ -27,4 +27,22 @@ console.log(string)
 // whenever we are type converting a variable 
 // type in which it will be converted is written 
 // started by capital letter 
-// like => Number, string , Boolean
+// like => Number, string ,Boolean
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+// In java script convertion like this is happened on the first input by default 
+
+console.log((3+4)* 5% 3)
+
+console.log(+true)
+
+let num1 ,num2, num3
+num1=num2=num3 = 2+2 // bad format behaviour
+let gamecounter= 100
+++ gamecounter 
+console.log(gamecounter)
+gamecounter ++;
+console.log(gamecounter)
